527d220fe1ec7026c60a716b68c9b5de2289d3d3
Ruben Gamez <ruben.gamez@ed.ac.uk>
2015-12-10 18:13:32 +0000

    Add record Model

---
+++ app/src/main/java/uk/ac/edina/fieldtriplite/model/RecordField.java
@@ -0,0 +1,55 @@
+package uk.ac.edina.fieldtriplite.model;
+
+public class RecordField {
+    private String id;
+    private Object val;
+    private String label;
+
+    /**
+     * Get the record field Id
+     * @return record field Id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Set the Id for the record field
+     * @param id of the record field
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * Get the value of the record field
+     * @return
+     */
+    public Object getVal() {
+        return val;
+    }
+
+    /**
+     * Get the value of the record field
+     * @param val value for the field
+     */
+    public void setVal(Object val) {
+        this.val = val;
+    }
+
+    /**
+     * Get the label for the record field
+     * @return label for the field
+     */
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * Set the label for the field
+     * @param label for the field
+     */
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
---
+++ app/src/main/java/uk/ac/edina/fieldtriplite/model/RecordModel.java
@@ -0,0 +1,47 @@
+package uk.ac.edina.fieldtriplite.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecordModel extends GeoJSONModel {
+    private String id;
+    private String name;
+    private List<RecordField> fields = new ArrayList<RecordField>();
+
+    public RecordModel() {
+        super();
+        properties.put("fields", fields);
+    }
+
+    /**
+     * Get the record Id
+     * @return record Id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Set the recordId
+     * @param recordId Record Id
+     */
+    public void setId(String recordId) {
+        id = recordId;
+    }
+
+    /**
+     * Add a field to the record
+     * @param field a record field
+     */
+    public void addRecordField(RecordField field) {
+        fields.add(field);
+    }
+
+    /**
+     * Get the list of field in the record
+     * @return list of fields in the record
+     */
+    public List<RecordField> getRecordFields() {
+        return fields;
+    }
+}
